6e2423ee242bb98355bafb46f2cd77da1ec2f16b
yook <si89336@naver.com>
2024-08-18 00:57:22 +0900

    ✨ Feat: pagination 구현

--- common.js
+++ common.js
@@ -6,6 +6,17 @@ let page = 1; //현재 페이지
 let totalResults = 0; //총 리스트 수
 let groupSize = 3; //한번에 보여지는 페이지
 
+const url = `http://openAPI.seoul.go.kr:8088/${API_KEY}/json/ListPublicReservationEducation/`;
+
+const searchParams = {
+  pageBegin: (page - 1) * pageSize + 1,
+  pageEnd: page * pageSize,
+  minclass: "", // category
+  svcname: "",
+  target: "", // not use
+  area: "",
+};
+
 //--------
 // ui
 const $logo = document.getElementById("logo");
@@ -63,28 +74,91 @@ window.addEventListener("resize", checkScreenSize);
 //--------
 // api
 const fetchList = async () => {
-  let serviceList = [];
-  const res = await fetch(
-    `http://openAPI.seoul.go.kr:8088/${API_KEY}/json/ListPublicReservationEducation/1/15/`
-  );
-  const data = await res.json();
+  //""이 falsy한 값이라 "%20"
+  const newUrl =
+    url +
+    Object.values(searchParams)
+      .map((param) => param || "%20")
+      .join("/");
 
-  totalResults = data.ListPublicReservationEducation.list_total_count;
-  serviceList = data.ListPublicReservationEducation.row;
+  console.log(newUrl);
 
-  console.log(data.ListPublicReservationEducation.row);
+  try {
+    let serviceList = [];
+    const res = await fetch(newUrl);
+    const data = await res.json();
 
-  renderList(serviceList);
+    totalResults = data.ListPublicReservationEducation.list_total_count;
+    serviceList = data.ListPublicReservationEducation.row;
+
+    console.log(data.ListPublicReservationEducation.row);
+
+    renderList(serviceList);
+    pagination();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const movePage = (pageNum) => {
+  //2페이지면 16에서 30
+  page = pageNum;
+  searchParams.pageBegin = (pageNum - 1) * pageSize + 1;
+  searchParams.pageEnd = pageNum * pageSize;
+  fetchList();
+};
+
+// movePage 함수를 window 객체에 추가
+window.movePage = movePage;
+
+const pagination = () => {
+  let pageGroup = Math.ceil(page / groupSize); // 현재 페이지가 속해있는 그룹
+  let firstPage = (pageGroup - 1) * groupSize + 1; //첫번째 페이지
+  let lastPage = Math.min(
+    Math.ceil(totalResults / pageSize),
+    pageGroup * groupSize
+  ); //마지막 페이지
+  let totalPage = Math.ceil(totalResults / pageSize); //총 페이지
+  let prevGroup = (pageGroup - 2) * groupSize + 1; //이전 페이지 그룹의 첫 페이지 계산
+  let nextGroup = pageGroup * groupSize + 1; //다음 페이즈 그룹의 첫 페이지 계산
+
+  let paginationHtml = `
+      <button class="arrow" ${
+        page === 1 ? "disabled" : ""
+      } onClick='movePage(${prevGroup})'>
+          <img src="./img/left.png" alt="이전 페이지로" />
+      </button>`;
+
+  for (let i = firstPage; i <= lastPage; i++) {
+    paginationHtml += `<button class="page ${
+      i === page ? "on" : ""
+    }" onClick='movePage(${i})'>${i}</button>`;
+  }
+
+  console.log(page);
+
+  paginationHtml += `
+      <button class="arrow" ${
+        page >= totalPage ? "disabled" : ""
+      } onClick='movePage(${nextGroup})'>
+          <img src="./img/right.png" alt="다음 페이지로" />
+      </button>`;
+
+  document.querySelector(".pagination").innerHTML = paginationHtml;
 };
 
 const createHtml = (service) => {
+  let title =
+    service.SVCNM.trim().length > 30
+      ? service.SVCNM.trim().substring(0, 31) + "..."
+      : service.SVCNM.trim();
   return `<div class="serviceItem">
               <div class="thumnailWrap">
                 <img src="${service.IMGURL}" alt-="" />
               </div>
               <div class="serviceDetailWrap">
                 <p class="serviceTitle">
-                  ${service.SVCNM.trim()}
+                  ${title}
                 </p>
                 <div class="detail">
                   <div class="userWrap">
